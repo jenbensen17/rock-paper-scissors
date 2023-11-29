37c5b0c80a950cb30f45d1ac5c8698769b072290
Benjamin Jensen <149121554+jenbensen17@users.noreply.github.com>
2023-11-29 16:04:29 -0600

    Add GUI and finish game features

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function playerWin() {
+    playerScore++;
+}
+
+function computerWin() {
+    computerScore++;
+}
+
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3)) + 1;
     switch(choice){
@@ -9,3 +20,53 @@ function getComputerChoice() {
                 break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection) {
+        return "It's a tie! Play again!";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
+    (playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "scissors" && computerSelection === "paper")) {
+        playerWin();
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        computerWin();
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
+
+function tieBreaker() {
+    console.log(playRound(prompt("Choose Your Weapon"),getComputerChoice())+"\n");
+    console.log("Player Score: "+playerScore+"\nComputer Score: "+computerScore+"\n");
+    getWinner();
+}
+
+function getWinner() {
+    if(playerScore > computerScore) {
+        console.log("\n\nPLAYER WINS BEST OUT OF 5");
+    } else if(playerScore === computerScore) {
+        console.log("\n\nTIE BREAKER NEEDED")
+        tieBreaker();
+
+    } else{
+        console.log("\n\nCOMPUTER WINS BEST OUT OF 5");
+    }
+    console.log("\n\n")
+}
+
+function game() {
+    playerScore =0;
+    computerScore = 0;
+    let rounds = 5;
+    while(rounds>0) {
+        console.log(playRound(prompt("Choose Your Weapon"),getComputerChoice())+"\n");
+        console.log("Player Score: "+playerScore+"\nComputer Score: "+computerScore+"\n");
+        rounds--;
+    }
+    getWinner();
+}
+
+
+
