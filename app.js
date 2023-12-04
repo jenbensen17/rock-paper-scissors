69c885567b27b243b02be6ece8d9dfb0bdf6754d
Benjamin Jensen <149121554+jenbensen17@users.noreply.github.com>
2023-12-04 12:18:52 -0600

    Display results and set score limit to end game

--- app.js
+++ app.js
@@ -11,53 +11,67 @@ function computerWin() {
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3)) + 1;
-    switch(choice){
-        case 1: return "rock";
-                break;
-        case 2: return "paper";
-                break;
-        case 3: return "scissors";
-                break;
+    switch(choice) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
-    if(playerSelection === computerSelection) {
-        return "It's a tie! Play again!";
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
-    (playerSelection === "paper" && computerSelection === "rock") || 
-    (playerSelection === "scissors" && computerSelection === "paper")) {
-        playerWin();
-        return "You Win! " + playerSelection + " beats " + computerSelection;
-    } else {
-        computerWin();
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    if(playerScore >=5 || computerScore>=5) {
+        getWinner();
+    }
+    else 
+    {    playerSelection = playerSelection.toLowerCase();
+        let roundResult = '';
+        if(playerSelection === computerSelection) {
+            roundResult = "It's a tie! Play again!";
+        } else if ((playerSelection === "rock" && computerSelection === "scissors") || 
+            (playerSelection === "paper" && computerSelection === "rock") || 
+            (playerSelection === "scissors" && computerSelection === "paper")) {
+            playerWin();
+            roundResult = "You Win! " + playerSelection + " beats " + computerSelection;
+        } else {
+            computerWin();
+            roundResult = "You Lose! " + computerSelection + " beats " + playerSelection;
+        }
+        toPrint.innerHTML = roundResult + "<br><br>" + getScore()+"<br><br>";
+        results.appendChild(toPrint.cloneNode(true));
+    }
+    if(playerScore >=5 || computerScore>=5) {
+        getWinner();
     }
 }
 
 function tieBreaker() {
-    console.log(playRound(prompt("Choose Your Weapon"),getComputerChoice())+"\n");
-    console.log("Player Score: "+playerScore+"\nComputer Score: "+computerScore+"\n");
+    console.log(playRound(prompt("Choose Your Weapon"), getComputerChoice()) + "\n");
+    console.log("Player Score: " + playerScore + "\nComputer Score: " + computerScore + "\n");
     getWinner();
+}
+
+function getScore() {
+    return "Player Score: " + playerScore + "<br>Computer Score: " + computerScore + "<br>";
 }
 
 function getWinner() {
     if(playerScore > computerScore) {
-        console.log("\n\nPLAYER WINS BEST OUT OF 5");
+        toPrint.textContent = ("PLAYER WINS BEST OUT OF 5");
     } else if(playerScore === computerScore) {
-        console.log("\n\nTIE BREAKER NEEDED")
+        toPrint.textContent = ("TIE BREAKER NEEDED");
         tieBreaker();
-
-    } else{
-        console.log("\n\nCOMPUTER WINS BEST OUT OF 5");
+    } else {
+        toPrint.textContent = ("COMPUTER WINS BEST OUT OF 5");
     }
-    console.log("\n\n")
+
+    results.appendChild(toPrint);
 }
 
 function game() {
-    playerScore =0;
+    playerScore = 0;
     computerScore = 0;
     
     getWinner();
@@ -67,12 +81,15 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
+const results = document.querySelector('#results');
+const toPrint = document.createElement("div");
+
 rock.addEventListener('click', () => {
-   console.log(playRound('rock',getComputerChoice()));
-})
+    playRound('rock', getComputerChoice());
+});
 paper.addEventListener('click', () => {
-    console.log(playRound('paper',getComputerChoice()));
- })
- scissors.addEventListener('click', () => {
-    console.log(playRound('paper',getComputerChoice()));
- })
+    playRound('paper', getComputerChoice());
+});
+scissors.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
